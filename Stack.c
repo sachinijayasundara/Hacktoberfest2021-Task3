75a856db23ecc20e4c1de0721d297cac5d27c5ec
sachinijayasundara <91739119+sachinijayasundara@users.noreply.github.com>
2021-10-03 16:29:12 +0530

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -8,7 +8,6 @@ int size,choice,ele;
 struct stack{
     int arr[100];
     int top;
-    int rear;
 
 }st;
 
@@ -21,9 +20,9 @@ void push(int element)
     }
     else
     {
-        st.top--;
+        st.top++;
         printf("\nEnter a Value ");
-        scanf("%s",&ele);
+        scanf("%d",&ele);
         st.arr[st.top]=ele;
     }
 }
@@ -48,7 +47,7 @@ int pop()
 int peek()
 {
     int display;
-    display=st.arr[st.rear];
+    display=st.arr[st.top];
     return display;
 }
 
@@ -58,7 +57,7 @@ void display()
     if((st.top)>=0)
     {
         printf("\n\nElements in the Stack");
-        for(i=st.top;i>=0;i++)
+        for(int i=st.top;i>=0;i--)
         {
             printf("\n%d",st.arr[i]);
         }
@@ -79,7 +78,7 @@ int main()
 
     do{
         printf("\nEnter Your Choice  ");
-        scanf("%c",&choice);
+        scanf("%d",&choice);
         switch(choice)
         {
         case 1:
@@ -88,11 +87,11 @@ int main()
             }
         case 2:
             {
-                printf("%d",pop());
+                printf("%d",pop());break;
             }
         case 3:
             {
-                printf("%d",peek());
+                printf("%d",peek());break;
             }
         case 4:
             {
@@ -107,5 +106,4 @@ int main()
         }
     }while(choice=5);
     return 0;
-   }
-
+}
